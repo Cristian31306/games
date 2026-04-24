3b16dc15742f37967242a20958cc1ab4358080e3
Cristian31306 <est_ca_duran@fesc.edu.co>
2026-04-24 13:46:39 -0500

    feat: add more words to Adivina and prevent repeats

--- games/adivina.js
+++ games/adivina.js
@@ -2,22 +2,47 @@ const ADIVINA_WORDS = {
     "Música": [
         "Shakira", "Karol G", "J Balvin", "Maluma", "Diomedes Díaz", 
         "Bad Bunny", "Feid", "Carlos Vives", "Juanes", "Daddy Yankee", 
-        "Luis Miguel", "Camilo", "Silvestre Dangond", "Ryan Castro", "Greeicy"
+        "Luis Miguel", "Camilo", "Silvestre Dangond", "Ryan Castro", "Greeicy",
+        "Blessd", "Sebastián Yatra", "Manuel Turizo", "Jorge Celedón", "Pipe Bueno",
+        "Dua Lipa", "Taylor Swift", "Rosalía", "Rauw Alejandro", "Bizarrap",
+        "Tiësto", "Avicii", "Joe Arroyo", "Grupo Niche", "Guayacán Orquesta"
     ],
     "TV y Cine": [
         "El Chavo del 8", "Betty la Fea", "Spider-Man", "Batman", "Pedro el Escamoso", 
         "Iron Man", "Jack Sparrow", "La Roca", "Amparo Grisales", "Suso el Paspi", 
-        "Joker", "Barbie", "Don Jediondo", "Piraña", "La Peliteñida"
+        "Joker", "Barbie", "Don Jediondo", "Piraña", "La Peliteñida",
+        "La Casa de Papel", "Stranger Things", "Harry Potter", "Titanic", "Avatar",
+        "Los Simpson", "Padre de Familia", "Shrek", "Toy Story", "Rey León",
+        "Cobra Kai", "Pasión de Gavilanes", "La vendedora de rosas", "Narcos", "Sin senos no hay paraíso"
     ],
     "Deportes": [
         "James Rodríguez", "Luis Díaz", "Falcao García", "Lionel Messi", "Cristiano Ronaldo", 
         "Egan Bernal", "Mariana Pajón", "Neymar", "Rigoberto Urán", "René Higuita", 
-        "Juanfer Quintero", "Pibe Valderrama", "Linda Caicedo", "Ronaldinho", "Usain Bolt"
+        "Juanfer Quintero", "Pibe Valderrama", "Linda Caicedo", "Ronaldinho", "Usain Bolt",
+        "Roger Federer", "Rafael Nadal", "Serena Williams", "LeBron James", "Stephen Curry",
+        "Pau Gasol", "Caterine Ibargüen", "David Ospina", "Yerry Mina", "Cuadrado",
+        "Checo Pérez", "Lewis Hamilton", "Tiger Woods", "Michael Jordan", "Kobe Bryant"
     ],
     "Tendencias": [
         "Elon Musk", "La Liendra", "Yeferson Cossio", "Westcol", "Bill Gates", 
         "Mark Zuckerberg", "Papa Francisco", "Donald Trump", "Epa Colombia", "Aida Victoria Merlano",
-        "MrBeast", "Ibai Llanos", "Luisito Comunica", "Juanda", "Pautips"
+        "MrBeast", "Ibai Llanos", "Luisito Comunica", "Juanda", "Pautips",
+        "Andrea Valdiri", "Felipe Saruma", "Dani Duke", "La Segura", "El Mindo",
+        "Tulio Recomienda", "Jh de la Cruz", "Kylie Jenner", "Kim Kardashian", "Kanye West"
+    ],
+    "Lugares": [
+        "Torre Eiffel", "Estatua de la Libertad", "Muralla China", "Coliseo Romano", "Machu Picchu",
+        "Cartagena", "Medellín", "Bogotá", "San Andrés", "Caño Cristales",
+        "Amazonas", "Piedra del Peñol", "Monserrate", "Parque Tayrona", "Nevado del Ruiz",
+        "París", "Nueva York", "Londres", "Tokio", "Dubái",
+        "Píramides de Egipto", "Cristo Redentor", "Gran Cañón", "Cataratas del Iguazú", "Disney World"
+    ],
+    "Comida": [
+        "Bandeja Paisa", "Ajiaco", "Sancocho", "Arepa con Queso", "Empanada",
+        "Tamal", "Lechona", "Mondongo", "Pizza", "Hamburguesa",
+        "Sushi", "Tacos", "Pasta", "Hot Dog", "Salchipapa",
+        "Chicharrón", "Buñuelo", "Natilla", "Pandebono", "Arroz con Pollo",
+        "Lasagna", "Burrito", "Kebab", "Paella", "Ceviche"
     ]
 };
 
@@ -39,7 +64,8 @@ function getOrCreateAdivinaRoom(roomId) {
             },
             countdownInterval: null,
             score: 0,
-            lastTurnResult: null
+            lastTurnResult: null,
+            usedWords: []
         });
     }
     return adivinaRooms.get(roomId);
@@ -88,9 +114,11 @@ export default function registerAdivinaHandlers(io, socket) {
 
             room.status = 'jugando';
             room.guesserId = playerIds[0];
+            room.usedWords = []; // Resetear palabras usadas al inicio de la partida
             
-            const words = ADIVINA_WORDS[room.settings.category] || ADIVINA_WORDS["Famosos"];
+            const words = ADIVINA_WORDS[room.settings.category] || ADIVINA_WORDS["Música"];
             room.currentWord = words[Math.floor(Math.random() * words.length)];
+            room.usedWords.push(room.currentWord);
             room.timer = room.settings.timerDuration;
 
             if (room.countdownInterval) clearInterval(room.countdownInterval);
@@ -150,8 +178,20 @@ export default function registerAdivinaHandlers(io, socket) {
             const nextIndex = (currentIndex + 1) % playerIds.length;
             room.guesserId = playerIds[nextIndex];
 
-            const words = ADIVINA_WORDS[room.settings.category] || ADIVINA_WORDS["Famosos"];
-            room.currentWord = words[Math.floor(Math.random() * words.length)];
+            const words = ADIVINA_WORDS[room.settings.category] || ADIVINA_WORDS["Música"];
+            
+            // Filtrar palabras que no se han usado
+            let availableWords = words.filter(w => !room.usedWords.includes(w));
+            
+            // Si todas se usaron, reiniciar lista
+            if (availableWords.length === 0) {
+                room.usedWords = [];
+                availableWords = words;
+            }
+
+            room.currentWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+            room.usedWords.push(room.currentWord);
+            
             room.timer = room.settings.timerDuration;
             room.status = 'jugando';
 
